7fa421876f264d383c6c21a04ca3099b32bbc550
astel <kiyomillefeuille@gmail.com>
2019-11-09 16:43:24 +0900

    リファクタ

--- src/main/scala/net/astail/Main.scala
+++ src/main/scala/net/astail/Main.scala
@@ -32,19 +32,47 @@ object Main {
     }
   }
 
-
   case class ResponseMessage(text: String, channel: String)
+
+  case class libraryMessage(channel: String, userId: String)
 
   def messageCheck(message: Message, client: SlackRtmClient): Option[ResponseMessage] = {
     val botChannel = ConfigFactory.load.getString("gether_times_post_slack_channel")
     val channel: String = client.state.getChannelIdForName(botChannel).getOrElse("")
 
-    if (message.channel != channel)
-      message.text match {
-        case "ping" => Some(ResponseMessage(s"<@${message.user}> pong", message.channel))
-        case _ => Some(ResponseMessage(s"<#${message.channel}> / <@${message.user}>: ${message.text}", channel))
-      }
-    else
-      None
+    // 自分のIDを設定して自分以外は反応しないようにする
+    val myUserId: String = "U054X0P0V"
+    val libraryChannel: String = client.state.getChannelIdForName("library").getOrElse("")
+    val lSetting = libraryMessage(libraryChannel, myUserId)
+
+    if (lSetting == libraryMessage(message.channel, message.user)) {
+      libraryMessageCheck(message, client, libraryChannel)
+    } else if (message.channel != channel)
+      if (message.channel != libraryChannel)
+        message.text match {
+          case "ping" => Some(ResponseMessage(s"<@${message.user}> pong", message.channel))
+          case _ => Some(ResponseMessage(s"<#${message.channel}> / <@${message.user}>: ${message.text}", channel))
+        }
+      else None
+    else None
+  }
+
+
+  def libraryMessageCheck(message: Message, client: SlackRtmClient, libraryChannel: String): Option[ResponseMessage] = {
+    val rentalArg = if (s"${message.text.head}" == "a")
+      "貸して"
+    else if (s"${message.text.head}" == "b")
+      "返す"
+
+    // 2.13からtoIntOptionが使えるけどslack-scala-client 0.2.7の調子が悪いため2.13に対応してないバーションのまま
+    def safeStringToInt(str: String): Option[Int] = {
+      import scala.util.control.Exception._
+      catching(classOf[NumberFormatException]) opt str.toInt
+    }
+
+    safeStringToInt(message.text.tail) match {
+      case Some(_: Int) => Some(ResponseMessage(s"<@UKE4U4LDD> --${message.text.tail} $rentalArg", libraryChannel))
+      case None => None
+    }
   }
 }
